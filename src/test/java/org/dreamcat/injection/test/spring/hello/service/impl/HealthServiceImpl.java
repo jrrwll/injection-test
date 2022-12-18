13194427e9f5323a696c2a874383d2c931afd66e
tuke <jrriwll@gmail.com>
2022-12-18 23:34:13 +0800

    commit

--- src/test/java/org/dreamcat/injection/test/spring/hello/service/impl/HealthServiceImpl.java
+++ src/test/java/org/dreamcat/injection/test/spring/hello/service/impl/HealthServiceImpl.java
@@ -1,6 +1,10 @@
 package org.dreamcat.injection.test.spring.hello.service.impl;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.dreamcat.injection.test.spring.hello.service.HealthService;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,5 +17,31 @@ public class HealthServiceImpl implements HealthService {
     @Override
     public void ping() {
         System.out.println("PONG");
+    }
+
+    @Getter
+    @Component
+    @RequiredArgsConstructor
+    public static class Jerry {
+        private final Tom tom;
+    }
+
+    @Getter
+    @Component
+    @RequiredArgsConstructor
+    public static class Tom {
+        private final Harry harry;
+    }
+
+    @Getter
+    @Component
+    public static class Harry {
+        private final Jerry jerry;
+
+
+        public Harry(
+                @Lazy Jerry jerry) {
+            this.jerry = jerry;
+        }
     }
 }
