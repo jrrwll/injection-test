7ed268d3a307f49efa64337b8efb0cc6174d68d9
tuke <jrriwll@gmail.com>
2025-09-14 19:57:50 +0800

    commit

--- src/main/java/org/dreamcat/injection/test/resolver/SpringTestExecutionListener.java
+++ src/main/java/org/dreamcat/injection/test/resolver/SpringTestExecutionListener.java
@@ -1,13 +1,17 @@
 package org.dreamcat.injection.test.resolver;
 
-
+import org.dreamcat.common.Pair;
+import org.dreamcat.common.function.TriFunction;
 import org.dreamcat.common.util.ArrayUtil;
 import org.dreamcat.common.util.ObjectUtil;
 import org.dreamcat.common.util.ReflectUtil;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.env.YamlPropertySourceLoader;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+import org.springframework.core.env.PropertySource;
 import org.springframework.core.env.StandardEnvironment;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.ResourcePropertySource;
 
 import java.io.IOException;
@@ -23,6 +27,7 @@ import java.util.function.Function;
  * @author Jerry Will
  * @version 2025-09-05
  */
+@SuppressWarnings({"unchecked", "rawtypes"})
 public class SpringTestExecutionListener extends InjectionTestExecutionListener {
 
     public SpringTestExecutionListener(Class<?> testClass, Map<String, String> properties) {
@@ -67,8 +72,33 @@ public class SpringTestExecutionListener extends InjectionTestExecutionListener 
     }
 
     /// parse expr vars
-    private static final List<String> extensions = Arrays.asList(
-            "yml", "yaml", "properties");
+    static TriFunction<String, String, String, List<PropertySource<?>>> yamlFn = (ext, prefix, profile) -> {
+        YamlPropertySourceLoader loader = new YamlPropertySourceLoader();
+
+        try {
+            String path = String.format("%sapplication%s.%s", prefix, profile, ext);
+            return loader.load(
+                    "application", new ClassPathResource(path));
+        } catch (IOException e) {
+            return Collections.emptyList();
+        }
+    };
+
+    static TriFunction<String, String, String, List<PropertySource<?>>> propsFn = (ext, prefix, profile) -> {
+        String location = String.format("classpath:%sapplication%s.%s", prefix, profile, ext);
+        try {
+            ResourcePropertySource propertySource = new ResourcePropertySource(location);
+            return Collections.singletonList(propertySource);
+        } catch (IOException e) {
+            return Collections.emptyList();
+        }
+    };
+
+    private static final List<Pair<List<String>, TriFunction<
+            String, String, String, List<PropertySource<?>>>>> extensions = Arrays.asList(
+            Pair.of(Arrays.asList("yml", "yaml"), yamlFn),
+            Pair.of(Collections.singletonList("properties"), propsFn)
+    );
 
     public static Environment createEnvironment() {
         StandardEnvironment env = new StandardEnvironment();
@@ -83,26 +113,22 @@ public class SpringTestExecutionListener extends InjectionTestExecutionListener 
     }
 
     private static void addSources(StandardEnvironment env, String profile) {
-        for (String ext : extensions) {
-            ResourcePropertySource cfgSource = resourcePropertySource(ext, "config/", profile);
-            if (cfgSource != null) {
-                env.getPropertySources().addLast(cfgSource);
-            }
-            ResourcePropertySource source = resourcePropertySource(ext, "", profile);
-            if (source != null) {
-                env.getPropertySources().addLast(source);
-            }
-            if (cfgSource != null || source != null) break;
-        }
-    }
+        for (Pair pair : extensions) {
+            List<String> extList = (List<String>) pair.first();
+            TriFunction<String, String, String, List<PropertySource<?>>> fn = (TriFunction) pair.second();
 
-    private static ResourcePropertySource resourcePropertySource(
-            String ext, String prefix, String profile) {
-        try {
-            return new ResourcePropertySource(String.format(
-                    "classpath:%sapplication%s.%s", prefix, profile, ext));
-        } catch (IOException e) {
-            return null;
+            for (String ext : extList) {
+                List<PropertySource<?>> cfgSources = fn.apply(ext, "config/", profile);
+                if (ObjectUtil.isNotEmpty(cfgSources)) {
+                    cfgSources.forEach(ps -> env.getPropertySources().addLast(ps));
+                }
+
+                List<PropertySource<?>> sources = fn.apply(ext, "", profile);
+                if (ObjectUtil.isNotEmpty(sources)) {
+                    sources.forEach(ps -> env.getPropertySources().addLast(ps));
+                }
+                if (ObjectUtil.isNotEmpty(cfgSources) || ObjectUtil.isNotEmpty(sources)) break;
+            }
         }
     }
 }
