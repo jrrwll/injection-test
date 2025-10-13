1fb17fde293d4d8263741c5887a76237cafff017
tuke <jrriwll@gmail.com>
2025-10-13 22:47:15 +0800

    commit

--- src/test/resources/service.java
+++ src/test/resources/service.java
@@ -1,45 +1,20 @@
 package $packageName.service;
 
-import $packageName.entity.$Entity;
 import java.util.List;
 import org.dreamcat.common.Pair;
+import $packageName.entity.$Entity;
 
-public interface $ {
+public interface ${Entity}Service {
 
-    Entity
+    Long create${Entity}($Entity entity);
+
+    ${Entity} delete${Entity}(Long id);
+
+    Long update${Entity}($Entity entity);
+
+    ${Entity} get${Entity}(Long id);
+
+    List<${Entity}> list${Entity}(String nameLike);
+
+    Pair<List<${Entity}>, Long> page${Entity}(String nameLike, Integer pageNum, Integer pageSize);
 }
-
-    Service {
-
-        Long create$ {
-            Entity
-        } ($Entity entity);
-
-        $ {
-            Entity
-        } delete$ {
-            Entity
-        } (Long id);
-
-        Long update$ {
-            Entity
-        } ($Entity entity);
-
-        $ {
-            Entity
-        } get$ {
-            Entity
-        } (Long id);
-
-        List < $ {
-            Entity
-        }>list$ {
-            Entity
-        } (String nameLike);
-
-        Pair < List < $ {
-            Entity
-        }>,Long > page$ {
-            Entity
-        } (String nameLike, Integer pageNum, Integer pageSize);
-    }
