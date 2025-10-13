a3f30a205fa6da5e8af0d160cafa48fd973543a2
tuke <jrriwll@gmail.com>
2025-10-13 22:40:30 +0800

    commit

---
+++ src/main/java/org/dreamcat/injection/test/resolver/ProfileExprValueResolver.java
@@ -0,0 +1,106 @@
+package org.dreamcat.injection.test.resolver;
+
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.dreamcat.common.Pair;
+import org.dreamcat.common.io.FileUtil;
+import org.dreamcat.common.json.JavaPropsUtil;
+import org.dreamcat.common.json.JsonUtil;
+import org.dreamcat.common.json.TomlUtil;
+import org.dreamcat.common.json.YamlUtil;
+import org.dreamcat.common.util.ClassLoaderUtil;
+import org.dreamcat.common.util.FunctionUtil;
+import org.dreamcat.common.util.MapUtil;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * @author Jerry Will
+ * @version 2025-09-05
+ */
+@Setter
+@Accessors(chain = true)
+public class ProfileExprValueResolver {
+
+    private String baseName = "application";
+    private String profileKey = "rita.profiles.active";
+    private String profileDir; // null means classpath
+
+    /// parse expr vars
+    private static final List<Pair<Function<String, Map<String, Object>>, List<String>>> extensions = Arrays.asList(
+            Pair.of(TomlUtil::fromJsonObject, Collections.singletonList("toml")),
+            Pair.of(YamlUtil::fromJsonObject, Arrays.asList("yml", "yaml")),
+            Pair.of(JsonUtil::fromJsonObject, Collections.singletonList("json")),
+            Pair.of(JavaPropsUtil::fromJsonObject, Collections.singletonList("properties"))
+    );
+
+    public Map<String, Object> parseExprVars() {
+        return parseExprVars(null);
+    }
+
+    public Map<String, Object> parseExprVars(String preferProfile) {
+        Map<String, Object> config = parseExprVars0(null);
+        String profile;
+        if (preferProfile == null) {
+            profile = (String) config.get(profileKey);
+            if (profile == null) return config;
+        } else {
+            profile = preferProfile;
+        }
+
+        Map<String, Object> profileConfig = parseExprVars0(profile);
+        MapUtil.merge(config, profileConfig);
+        return config;
+    }
+
+    private Map<String, Object> parseExprVars0(String profile) {
+        for (Pair<Function<String, Map<String, Object>>, List<String>> pair : extensions) {
+            Function<String, Map<String, Object>> mapper = pair.first();
+            List<String> extensions = pair.second();
+            for (String extension : extensions) {
+                String path;
+                if (profile != null) {
+                    path = String.format("%s-%s.%s", baseName, profile, extension);
+                } else {
+                    path = baseName + "." + extension;
+                }
+
+                String configContent = readConfigContent("config/" + path);
+                String content = readConfigContent(path);
+
+                Map<String, Object> configContentMap = new HashMap<>();
+                if (configContent != null) {
+                    configContentMap = MapUtil.flat(mapper.apply(configContent));
+                }
+
+                Map<String, Object> contentMap = new HashMap<>();
+                if (content != null) {
+                    contentMap = MapUtil.flat(mapper.apply(content));
+                }
+                if (contentMap.isEmpty() && configContentMap.isEmpty()) {
+                    continue;
+                }
+                MapUtil.merge(contentMap, configContentMap);
+                return contentMap;
+            }
+        }
+        return new HashMap<>();
+    }
+
+    private String readConfigContent(String path) {
+        if (profileDir == null) {
+            return FunctionUtil.invokeOrNull(() ->
+                    ClassLoaderUtil.getResourceAsString(path));
+        } else {
+            File profileFile = new File(profileDir, path);
+            if (!profileFile.exists()) return null;
+            return FunctionUtil.invokeOrNull( () -> FileUtil.readAsString(profileFile));
+        }
+    }
+}
--- src/main/java/org/dreamcat/injection/test/resolver/RitaTestExecutionListener.java
+++ src/main/java/org/dreamcat/injection/test/resolver/RitaTestExecutionListener.java
@@ -1,18 +1,5 @@
 package org.dreamcat.injection.test.resolver;
 
-import org.dreamcat.common.Pair;
-import org.dreamcat.common.json.JavaPropsUtil;
-import org.dreamcat.common.json.JsonUtil;
-import org.dreamcat.common.json.TomlUtil;
-import org.dreamcat.common.json.YamlUtil;
-import org.dreamcat.common.util.ClassLoaderUtil;
-import org.dreamcat.common.util.FunctionUtil;
-import org.dreamcat.common.util.MapUtil;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
@@ -34,61 +21,8 @@ public class RitaTestExecutionListener extends InjectionTestExecutionListener {
 
     @Override
     protected Function<String, Object> resolveExprValueProviderForConfig() {
-        Map<String, Object> exprVars = parseExprVars();
+        ProfileExprValueResolver exprValueResolver = new ProfileExprValueResolver();
+        Map<String, Object> exprVars = exprValueResolver.parseExprVars();
         return exprVars::get;
-    }
-
-    /// parse expr vars
-    static final List<Pair<Function<String, Map<String, Object>>, List<String>>> extensions = Arrays.asList(
-            Pair.of(TomlUtil::fromJsonObject, Collections.singletonList("toml")),
-            Pair.of(YamlUtil::fromJsonObject, Arrays.asList("yml", "yaml")),
-            Pair.of(JsonUtil::fromJsonObject, Collections.singletonList("json")),
-            Pair.of(JavaPropsUtil::fromJsonObject, Collections.singletonList("properties"))
-    );
-
-    public static Map<String, Object> parseExprVars() {
-        Map<String, Object> config = parseExprVars(null);
-        String profile = (String) config.get("rita.profiles.active");
-        if (profile == null) return config;
-
-        Map<String, Object> profileConfig = parseExprVars(profile);
-        MapUtil.merge(config, profileConfig);
-        return config;
-    }
-
-    private static Map<String, Object> parseExprVars(String profile) {
-        for (Pair<Function<String, Map<String, Object>>, List<String>> pair : extensions) {
-            Function<String, Map<String, Object>> mapper = pair.first();
-            List<String> extensions = pair.second();
-            for (String extension : extensions) {
-                String path;
-                if (profile != null) {
-                    path = String.format("application-%s.%s", profile, extension);
-                } else {
-                    path = "application." + extension;
-                }
-
-                String configContent = FunctionUtil.invokeOrNull(() ->
-                        ClassLoaderUtil.getResourceAsString("config/" + path));
-                String content = FunctionUtil.invokeOrNull(() ->
-                        ClassLoaderUtil.getResourceAsString(path));
-
-                Map<String, Object> configContentMap = new HashMap<>();
-                if (configContent != null) {
-                    configContentMap = MapUtil.flat(mapper.apply(configContent));
-                }
-
-                Map<String, Object> contentMap = new HashMap<>();
-                if (content != null) {
-                    contentMap = MapUtil.flat(mapper.apply(content));
-                }
-                if (contentMap.isEmpty() && configContentMap.isEmpty()) {
-                    continue;
-                }
-                MapUtil.merge(contentMap, configContentMap);
-                return contentMap;
-            }
-        }
-        return new HashMap<>();
     }
 }
